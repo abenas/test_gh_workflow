58772eb396d067f7a3065deda6a4685825417969
Abenadal Gomes <abenadal.gomes@gmail.com>
2023-12-22 14:01:16 -0300

    added linter

---
+++ pr-lint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    validLabels: [
+      "bug",
+      "skip-changelog",
+      "enhancement",
+      "feature"
+    ],
+    mandatorySections: [
+      {
+        beginsWith: "Changelog",
+        endsWith: "End of changelog",
+        message: "Changelog section is mandatory",
+        validate: (section) => {
+          return true;
+        }
+      }
+    ]
+  }
